ddf8997aa950fd4c9827e69e38fed5b3378c886a
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-05-27 23:50:25 +0100

    Fix rebase

--- test/polling_to_socket/periodic_test.exs
+++ test/polling_to_socket/periodic_test.exs
@@ -2,7 +2,6 @@ defmodule PollingToSocket.PeriodicTest do
   use ExUnit.Case, async: true
 
   test "Basic Behaviour" do
-<<<<<<< HEAD
     caller = self()
     random_number = :rand.uniform(50)
 
@@ -16,23 +15,5 @@ defmodule PollingToSocket.PeriodicTest do
     Enum.each(1..random_number, fn(x) ->
       assert_receive("done", 11 * x)
     end)
-=======
-    caller = self
-    args = %{interval: 100, work: fn ->
-      spawn_link(fn ->
-        send(caller, "done")
-      end)
-    end, id: self()}
-    GenServer.start_link(PollingToSocket.Periodically, args)
-
-    assert_receive("done", 150)
-
-    assert_receive("done", 150)
-    assert_receive("done", 150)
-    assert_receive("done", 150)
-    assert_receive("done", 150)
-
->>>>>>> cf3d0b1 (Adding periodic tests)
-
   end
 end
--- test/polling_to_socket_web/channels/poll_channel_test.exs
+++ test/polling_to_socket_web/channels/poll_channel_test.exs
@@ -1,16 +1,3 @@
 defmodule PollingToSocketWeb.PollChannelTest do
   use PollingToSocketWeb.ChannelCase, async: true
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 731f0f9 (Add interactive tests)
-=======
-=======
-
->>>>>>> 825b5c9 (Adding periodic tests)
->>>>>>> cf3d0b1 (Adding periodic tests)
-=======
->>>>>>> 34a8646 (Adding periodic tests)
 end
