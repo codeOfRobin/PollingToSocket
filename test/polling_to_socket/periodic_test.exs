5684c2943a6ac4ec250753bc6885f03c567896b5
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-05-27 23:47:40 +0100

    Update tests

--- test/polling_to_socket/periodic_test.exs
+++ test/polling_to_socket/periodic_test.exs
@@ -2,13 +2,19 @@ defmodule PollingToSocket.PeriodicTest do
   use ExUnit.Case, async: true
 
   test "Basic Behaviour" do
-    caller = self
-    args = %{interval: 100, work: fn ->
+    caller = self()
+    random_number = :rand.uniform(50)
+
+    # Run a random number of cycles and wait
+    args = %{interval: 10, work: fn ->
         send(caller, "done")
     end, id: self()}
     GenServer.start_link(PollingToSocket.Periodically, args)
 
-    assert_receive("done", 150)
+    # Wait > 10ms for the response
+    Enum.each(1..random_number, fn(x) ->
+      assert_receive("done", 11 * x)
+    end)
 
   end
 end
