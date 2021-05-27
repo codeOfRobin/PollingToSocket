3e46548ffb74baac8eb052229178dc3b3df5805d
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-05-27 23:41:08 +0100

    Update gitignore

--- test/polling_to_socket/periodic_test.exs
+++ test/polling_to_socket/periodic_test.exs
@@ -4,19 +4,11 @@ defmodule PollingToSocket.PeriodicTest do
   test "Basic Behaviour" do
     caller = self
     args = %{interval: 100, work: fn ->
-      spawn_link(fn ->
         send(caller, "done")
-      end)
     end, id: self()}
     GenServer.start_link(PollingToSocket.Periodically, args)
 
     assert_receive("done", 150)
-
-    assert_receive("done", 150)
-    assert_receive("done", 150)
-    assert_receive("done", 150)
-    assert_receive("done", 150)
-
 
   end
 end
