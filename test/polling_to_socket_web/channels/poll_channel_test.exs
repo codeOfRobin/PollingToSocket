d5d203e4647ce9027efaa3c8fae8490c09e57c42
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-05-27 23:49:58 +0100

    Adding periodic tests

--- test/polling_to_socket_web/channels/poll_channel_test.exs
+++ test/polling_to_socket_web/channels/poll_channel_test.exs
@@ -1,7 +1,13 @@
 defmodule PollingToSocketWeb.PollChannelTest do
   use PollingToSocketWeb.ChannelCase, async: true
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 731f0f9 (Add interactive tests)
+=======
+=======
+
+>>>>>>> 825b5c9 (Adding periodic tests)
+>>>>>>> cf3d0b1 (Adding periodic tests)
 end
