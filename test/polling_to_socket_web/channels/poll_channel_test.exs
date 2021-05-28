505928f3e9e14b21710ebab82269a60f6faaf414
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-05-28 01:51:59 +0100

    Add channel tests

--- test/polling_to_socket_web/channels/poll_channel_test.exs
+++ test/polling_to_socket_web/channels/poll_channel_test.exs
@@ -1,3 +1,16 @@
 defmodule PollingToSocketWeb.PollChannelTest do
   use PollingToSocketWeb.ChannelCase, async: true
+
+  describe "testing joining channel" do
+    test "joining" do
+      {:ok, _, socket} = join_channel("poll:12345")
+    end
+  end
+
+
+  defp join_channel(topic) do
+    PollingToSocketWeb.UserSocket
+    |> socket()
+    |> subscribe_and_join(PollingToSocketWeb.PollChannel, topic)
+  end
 end
