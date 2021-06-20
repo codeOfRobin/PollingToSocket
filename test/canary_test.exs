6240dcc8fceacfac3b4784e47b8867fc74621974
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-06-20 02:06:50 +0100

    Cleanup test

--- test/canary_test.exs
+++ test/canary_test.exs
@@ -1,7 +1,9 @@
-defmodule PollingToSocket.CanaryTest do
+defmodule PollingToSocketWeb.CanaryTest do
   use ExUnit.Case, async: true
 
-  test "make sure nothing is absolutely borked" do
-    assert 1 + 1 == 2
+  describe "Making sure testing suite is properly set up" do
+    test "addition" do
+      assert 1 + 1 == 2
+    end
   end
 end
--- test/polling_to_socket_web/base_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule PollingToSocketWeb.BaseTest do
-  use ExUnit.Case, async: true
-
-  describe "Making sure testing suite is properly set up" do
-    test "addition" do
-      assert 1 + 1 == 2
-    end
-  end
-end
